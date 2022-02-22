439a4a2b1d4c371f1f28be2c3a3ac7f18d654891
acceberc <rebecca-cunha@hotmail.com>
2022-02-22 20:43:34 -0300

    ex 32

---
+++ Exercicios resolvidos/ex32.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class ex32 {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		Random gerador = new Random();
+		
+		System.out.println("Informe um número de 1 a 5: ");
+		int numero = leia.nextInt();
+		
+
+		
+
+	}
+
+}
